9cf6ef23ad43c657ad6c7663adef265a2a65944a
田中慎吾 <tanakashingo@mbp.local>
2020-05-14 17:47:15 +0900

    finish chapter4

---
+++ src/Chapter04.java
@@ -0,0 +1,17 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+
+	//	変数宣言
+	String name1;
+	final int LUCKY_NUMBER = 7;
+
+	name1 = "Shingo tanaka";
+	System.out.println(name1);
+	//  再代入して出力
+	name1 = "Henkou Namae";
+	System.out.println(name1);
+
+	System.out.println(LUCKY_NUMBER);
+	}
+}
