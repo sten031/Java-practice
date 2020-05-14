de08e95d2ac80cafb868277ec7c11cbce2e0d2fe
田中慎吾 <tanakashingo@mbp.local>
2020-05-14 18:12:59 +0900

    finish Chapter05

---
+++ src/Chapter05.java
@@ -0,0 +1,25 @@
+public class Chapter05 {
+    public static void main(String[] args) {
+//    	大きい整数
+        long l = 1111111111111111111L;
+        System.out.println(l);
+//        浮動少数
+        double d = 5.55;
+        System.out.println(d);
+//        論理型
+        boolean b = true;
+        System.out.println(b);
+
+        Integer i = 100;
+        System.out.println(i);
+
+        Float f = 1.111f;
+        System.out.println(f);
+
+        Character c = 'あ';
+        System.out.println(c);
+
+        int[] array = { 1, 2, 3, 4, 5 };
+        System.out.println(array[3]);
+    }
+}
